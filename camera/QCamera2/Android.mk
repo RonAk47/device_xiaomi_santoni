4854dad6fabfe73d378e69665fa7218c2ea20877
RonAk47 <rjronakjain9@gmail.com>
2018-07-07 11:04:07 +0000

    Xtended Bringup & Some config changes

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -28,7 +28,7 @@ LOCAL_SRC_FILES += \
         HAL3/QCamera3CropRegionMapper.cpp \
         HAL3/QCamera3StreamMem.cpp
 
-LOCAL_CFLAGS := -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS := -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-variable -Wno-maybe-uninitialized -Wno-unused-result
 
 #HAL 1.0 source
 
